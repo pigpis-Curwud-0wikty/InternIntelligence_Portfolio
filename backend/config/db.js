ae048a3dbe5fc197a6c4ba3bf4aaec011d1240a1
ziadmohamed <ziad@gmail.com>
2025-11-29 14:00:22 +0200

    last-update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,13 @@
-```javascript
 const mongoose = require('mongoose');
 
-let isConnected = false;
-
 const connectDB = async () => {
-    mongoose.set('strictQuery', true);
-
-    if (isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
-
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI);
-        isConnected = db.connections[0].readyState;
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
     } catch (error) {
-        console.error("MongoDB connection error:", error);
+        console.log(error);
+        process.exit(1);
     }
 }
 
 module.exports = connectDB;
-```
