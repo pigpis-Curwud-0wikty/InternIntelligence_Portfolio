74d19f8a1a1fa7f2f39130f2644ecdbd56a4d9ea
ziadmohamed <ziad@gmail.com>
2025-11-29 13:24:07 +0200

    last-update

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,10 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
-    }catch(error){
-        console.log(error);
-        process.exit(1);
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        // Do not exit process in serverless environment
+        // process.exit(1); 
     }
 }
 
