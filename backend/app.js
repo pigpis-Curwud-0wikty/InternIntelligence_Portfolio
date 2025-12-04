a7509441b561650b67424777be0ef03a68912bd4
ziadmohamed <ziad@gmail.com>
2025-12-04 02:12:47 +0200

    last-update

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: allowedOrigins,
+    origin: true, // Allow all origins (reflects the request origin)
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma'],
